dae4e8609fa64a28d7bcb38ac1f75c901b172173
napensii <insk0005@yandex.ru>
2018-09-05 19:13:40 +0300

    1
    1

--- wurfl.js
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 - ScientiaMobile, Inc., Reston, VA
-// WURFL Device Detection
-// Terms of service:
-// http://web.wurfl.io/license
-
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('c b={"a":9,"8":"7 6-5 (4 3 2)","1":"0"};',13,13,'Smartphone|form_factor|Prime|Core|Galaxy|G360T1|SM|Samsung|complete_device_name|true|is_mobile|WURFL|var'.split('|'),0,{}))
